4609d58ee5c6da34513d7bef345b7d99b2c24a0e
quoclam204 <nguyenlequoclam@gmail.com>
2026-04-04 18:51:25 +0700

    Update checkout

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -3,80 +3,145 @@ using EcommerceMVC.Helpers;
 using EcommerceMVC.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TechGearShop.ViewModels;
 
 namespace EcommerceMVC.Controllers
 {
-	public class CartController : Controller
-	{
-		private readonly Hshop2023Context db;
+    public class CartController : Controller
+    {
+        private readonly Hshop2023Context db;
 
-		public CartController(Hshop2023Context context)
-		{
-			db = context;
-		}
+        public CartController(Hshop2023Context context)
+        {
+            db = context;
+        }
 
-		const string CART_KEY = "MYCART";
-		public List<CartItem> Cart => HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();
+        const string CART_KEY = "MYCART";
+        public List<CartItem> Cart => HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();
 
-		public IActionResult Index()
-		{
-			return View(Cart);
-		}
+        public IActionResult Index()
+        {
+            return View(Cart);
+        }
 
-		[Authorize]
-		public IActionResult AddToCart(int id, int quantity = 1)
-		{
-			var gioHang = Cart;
-			var item = gioHang.SingleOrDefault(p => p.MaHh == id);
-			if (item == null)
-			{
-				var hangHoa = db.HangHoas.SingleOrDefault(p => p.MaHh == id);
-				if (hangHoa == null)
-				{
-					TempData["Message"] = $"Không tìm thấy hàng hóa có mã {id}";
-					return Redirect("/404");
-				}
-				item = new CartItem
-				{
-					MaHh = hangHoa.MaHh,
-					TenHH = hangHoa.TenHh,
-					DonGia = hangHoa.DonGia ?? 0,
-					Hinh = hangHoa.Hinh ?? string.Empty,
-					SoLuong = quantity
-				};
-				gioHang.Add(item);
-			}
-			else
-			{
-				item.SoLuong += quantity;
-			}
+        [Authorize]
+        public IActionResult AddToCart(int id, int quantity = 1)
+        {
+            var gioHang = Cart;
+            var item = gioHang.SingleOrDefault(p => p.MaHh == id);
+            if (item == null)
+            {
+                var hangHoa = db.HangHoas.SingleOrDefault(p => p.MaHh == id);
+                if (hangHoa == null)
+                {
+                    TempData["Message"] = $"Không tìm thấy hàng hóa có mã {id}";
+                    return Redirect("/404");
+                }
+                item = new CartItem
+                {
+                    MaHh = hangHoa.MaHh,
+                    TenHH = hangHoa.TenHh,
+                    DonGia = hangHoa.DonGia ?? 0,
+                    Hinh = hangHoa.Hinh ?? string.Empty,
+                    SoLuong = quantity
+                };
+                gioHang.Add(item);
+            }
+            else
+            {
+                item.SoLuong += quantity;
+            }
 
-			HttpContext.Session.Set(MySetting.CART_KEY, gioHang);
+            HttpContext.Session.Set(MySetting.CART_KEY, gioHang);
 
-			return RedirectToAction("Index");
-		}
+            return RedirectToAction("Index");
+        }
 
-		public IActionResult RemoveCart(int id)
-		{
-			var gioHang = Cart;
-			var item = gioHang.SingleOrDefault(p => p.MaHh == id);
-			if (item != null)
-			{
-				gioHang.Remove(item);
-				HttpContext.Session.Set(MySetting.CART_KEY, gioHang);
-			}
-			return RedirectToAction("Index");
-		}
+        public IActionResult RemoveCart(int id)
+        {
+            var gioHang = Cart;
+            var item = gioHang.SingleOrDefault(p => p.MaHh == id);
+            if (item != null)
+            {
+                gioHang.Remove(item);
+                HttpContext.Session.Set(MySetting.CART_KEY, gioHang);
+            }
+            return RedirectToAction("Index");
+        }
 
-		[Authorize]
-		public IActionResult Checkout()
-		{
-			if (Cart.Count == 0)
-			{
-				TempData["Message"] = "Giỏ hàng của bạn đang trống.";
-				return RedirectToAction("Index");
-			}
-			return View(Cart);
+        [Authorize]
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            if (Cart.Count == 0)
+            {
+                TempData["Message"] = "Giỏ hàng của bạn đang trống.";
+                return RedirectToAction("Index");
+            }
+            return View(Cart);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Checkout(CheckoutVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var customerId = HttpContext.User.Claims.SingleOrDefault(User => User.Type ==
+                MySetting.CLAIM_CUSTOMERID).Value;
+
+                var khachHang = new KhachHang();
+
+                if (model.GiongKhachHang)
+                {
+                    khachHang = db.KhachHangs.SingleOrDefault(kh => kh.MaKh == customerId);
+                }
+
+                var hoaDon = new HoaDon
+                {
+                    MaKh = customerId,
+                    HoTen = model.HoTen ?? khachHang.HoTen,
+                    DiaChi = model.DiaChi ?? khachHang.DiaChi,
+                    DienThoai = model.DienThoai ?? khachHang.DienThoai,
+                    NgayDat = DateTime.Now,
+                    CachThanhToan = "COD",
+                    CachVanChuyen = "GRAB",
+                    MaTrangThai = 0,
+                    GhiChu = model.GhiChu
+                };
+
+                db.Database.BeginTransaction();
+                try
+                {
+                    db.Database.CommitTransaction();
+                    db.Add(hoaDon);
+                    db.SaveChanges();   
+
+                    var cthds = new List<ChiTietHd>();
+                    foreach (var item in Cart)
+                    {
+                        cthds.Add(new ChiTietHd
+                        {
+                            MaHd = hoaDon.MaHd,
+                            MaHh = item.MaHh,
+                            SoLuong = item.SoLuong,
+                            DonGia = item.DonGia,
+                            GiamGia = 0
+                        });
+                    }
+                    db.AddRange(cthds);
+                    db.SaveChanges();
+
+                    HttpContext.Session.Set<List<CartItem>>(MySetting.CART_KEY, new List<CartItem>());
+
+                    return View("Success"); 
+                }
+                catch
+                {
+                    db.Database.RollbackTransaction();
+                }
+            }
+            return View(Cart);
         }
     }
 }
---
+++ Data/HoaDon.Partial.cs
@@ -0,0 +1,6 @@
+namespace EcommerceMVC.Data;
+
+public partial class HoaDon
+{
+    public string? DienThoai { get; set; }
+}
